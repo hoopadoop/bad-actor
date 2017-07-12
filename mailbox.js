e87de98082d8d3e10d686f31470cc99f0612a293
hoopadoop <stevehooley@hotmail.com>
2017-07-12 17:52:00 +0100

    Update mailbox.js

--- mailbox.js
+++ mailbox.js
@@ -1,5 +1,4 @@
 /* @flow */
-import _ from 'underscore'
 import matches from './pattern_match'
 
 type MsgT = any
@@ -21,14 +20,31 @@ export default class Mailbox {
     if (!set.length) throw new Error('cannot match nothing')
     if (!this.mbox.length) return null
     const topMsg = this.mbox.shift()
-    const matchedIndex = _.findIndex(set, (msgFromSet) => (matches(msgFromSet, topMsg)))
+    const matchedIndex = set.findIndex(msgFromSet => matches(msgFromSet, topMsg))
     if (matchedIndex !== -1) {
-      this.mbox = [...this.savedMsgs, ...this.mbox]
-      this.savedMsgs = []
+      this.restoreSavedMsgs()
       return [topMsg, matchedIndex]
     } else {
       this.savedMsgs.push(topMsg)
       return this.match(set)
     }
+  }
+
+  _msgs(): Array<MsgT> {
+    return this.mbox
+  }
+
+  _savedMsgs(): Array<MsgT> {
+    return this.savedMsgs
+  }
+
+  restoreSavedMsgs(): void {
+    this.mbox = [...this.savedMsgs, ...this.mbox]
+    this.savedMsgs = []
+  }
+
+  debugMsgs(): string {
+    var all = [...this.savedMsgs, ...this.mbox]
+    return all.length ? JSON.stringify(all) : ''
   }
 }
